723941ac5041867b697986d0b827ddd03e736cdd
Arthur-LeSayec <arthur.lesayec@gmail.com>
2020-11-18 17:46:29 +0100

    Update Tic_Tac_Toe.py

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,4 +1,13 @@
 def initialiseGrille (Grille): 
     compteur = 0
     for compteur in range (0, 8) :
-        grille 
+        grille [compteur] = "_"
+    return grille
+
+def afficherGrille (grille):
+    i=0
+    for i in range (0, 2) :
+        print(grille[3*i], grille[3*i+1], grille[3*i+2], "\n")
+    return grille
+
+def ajouteSymbole
